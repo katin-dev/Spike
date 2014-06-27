d36171e66c138649d12f953bd0469420ec921768
sergey <gourry@mail.ru>
2014-06-27 15:42:01 +0400

    Избавился от переменных класса в пользу переменных метода.

--- Parser.php
+++ Parser.php
@@ -9,14 +9,7 @@ require_once "Lexema/Condition.php";
 require_once "Lexema/Loop.php";
 
 class Parser {
-	
-	/**
-	 * Распарсить переданный шаблон на основе переданных данных
-	 * ! К сожалению, из-за того, что в нашей CMS используется один экземпляр объекта Parser, нельзя использовать переменные класса для управления потоком. Все переменные ($data, $content, $position) должны быть локальными переменными метода.
-	 * @param string $content
-	 * @param array $data
-	 * @return string
-	 */
+
 	public function parse($content, $data) {
 		
 		$position = 0;
