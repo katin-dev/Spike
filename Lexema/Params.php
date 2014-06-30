2b311b04fc0a76c1736d14072edf4ddf57b831b6
sergey <gourry@mail.ru>
2014-06-30 10:59:11 +0400

    Добавил каркас для класса параметров Params.php

---
+++ Lexema/Params.php
@@ -0,0 +1,37 @@
+<?php
+
+class Lexema_Params 
+{
+	private $paramString;
+	public function __construct($string) {
+		$this->paramString = $string;
+	}
+	
+	public function getString() {
+		return $this->paramString;
+	}
+	
+	/**
+	 * Получить список переданных параметров
+	 * @return array key-value массив
+	 */
+	public function getParams($data) {
+		$params = array();
+		preg_match_all('#([-_\w]+)\s*=\s*"([^"]+)"#ims', $this->getString(), $m);
+		if(!empty($m[0])) {
+	
+			$keys = $m[1];
+			$values = $m[2];
+	
+			$params =  array();
+			foreach ($keys as $i => $key) {
+				if(preg_match('/^{(.*)}$/', $values[$i], $m)) {
+					$params[$key] = Lexema::getVariableValue($m[1], $data);
+				} else {
+					$params[$key] = $values[$i];
+				}
+			}
+		}
+		return $params;
+	}
+}
